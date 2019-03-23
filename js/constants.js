54879b1d8d79f97fcc6aa96bd91274cfd7b6e1c2
sungtaokao <paulkao94@gmail.com>
2019-03-22 21:45:56 -0700

    Structural changes

---
+++ js/constants.js
@@ -0,0 +1,15 @@
+var about1 = 'Molded in the sepulcher of rock n roll, the virile quadruplicate known as Mystryss emerged late in the vernal equinox of two-thousand eighteen to unleash their visceral brand of mutant melodies upon the unsuspecting populace of Vancouver.';
+
+var about2 = 'The legacy of this tenacious tetrad of tunesmiths was borne out of a serendipitous confluence, precipitated by Craigslist, between the leviathan literary architect and chanteur, known by his terrestrial name as Steven Travis, and the peerless plank-spanker known as Jeff Ogilvie. Throughout numerous lunations, the necrotic carapace of their deposed deity was pruned to reveal a torrid cataclysm of electrifying hymns. "We thought it sounded OK," says Travis while Ogilvie nods his head in agreement.';
+
+var about3 = 'While the people waited listfully to hear their latest enchanting ditty, the deadly dyad was working diligently recruiting bassist Paul Kao, and drummer Victor Bruyere to assist them on their grand undertaking. "The rhythms filled out our tunes like cornbread and beans fill out my lady\'s jeans," says Travis.';
+
+var about4 = 'With their collective firmly established the four have endeavored to digitally transmute their annihilating anthems and astound audiences throughout the third dimension.';
+
+var main = 'Check out Vancouver\'s latest and greatest original hard rock band! Coming to a venue near you! Stay tuned...'
+
+document.getElementById("about1").innerHTML = about1;
+document.getElementById("about2").innerHTML = about2;
+document.getElementById("about3").innerHTML = about3;
+document.getElementById("about4").innerHTML = about4;
+document.getElementById("main").innerHTML = main;
