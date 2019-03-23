19af3e05ca9409b7d92b901771024c1422aeec99
sungtaokao <paulkao94@gmail.com>
2019-03-23 15:10:32 -0700

    migrated contact and signup pages

--- js/demo_list.js
+++ js/demo_list.js
@@ -1,11 +1,15 @@
-var demolist = ['audio\\Six String Itch.mp3', 'audio\\Sweet Summer Days.mp3', '..\\audio\\The Saint.mp3', '..\\audio\\Vicious.mp3'];
+var demolist = ['Six String Itch.mp3',
+                'Sweet Summer Days.mp3',
+                'The Saint.mp3',
+                'Vicious.mp3'];
+var demodesc = [''];
 
 var createdDivs = '';
 var count = 1;
 
 demolist.forEach(demo => {
     
-    createdDivs += `<div class="container"><div class="row"><div class="col-lg-8 mx-auto"><button class="btn btn-info" data-toggle="collapse" data-target="#d${count}">Audio test element<br>Click Me!</button></div></div><div class="row collapse" id="d${count}"><div class="col-lg-8 mx-auto"><audio src="${demo}" controls/></div><div class="col-lg-8 mx-auto"><p>description</p></div></div></div>`;
+    createdDivs += `<div class="container"><div class="row"><div class="col-lg-8 mx-auto"><button class="btn btn-info" data-toggle="collapse" data-target="#d${count}">Audio test element<br>Click Me!</button></div></div><div class="row collapse" id="d${count}"><div class="col-lg-8 mx-auto"><audio src="audio\\${demo}" controls/></div><div class="col-lg-8 mx-auto"><p>description</p></div></div></div>`;
     
     count ++;
 });
---
+++ sections/contact.js
@@ -0,0 +1,57 @@
+var insert = `<section class="contact-section bg-black">` +
+  `<div class="container">` +
+
+    `<div class="row">` +
+
+      `<div class="col-md-4 mb-3 mb-md-0">` +
+        `<div class="card py-4 h-100">` +
+          `<div class="card-body text-center">` +
+            `<i class="fas fa-map-marked-alt text-primary mb-2"></i>` +
+            `<h4 class="text-uppercase m-0">Address</h4>` +
+            `<hr class="my-4">` +
+            `<div class="small text-black-50">123 Sesame St, NY, New York</div>` +
+          `</div>` +
+        `</div>` +
+      `</div>` +
+
+      `<div class="col-md-4 mb-3 mb-md-0">` +
+        `<div class="card py-4 h-100">` +
+          `<div class="card-body text-center">` +
+            `<i class="fas fa-envelope text-primary mb-2"></i>` +
+            `<h4 class="text-uppercase m-0">Email</h4>` +
+            `<hr class="my-4">` +
+            `<div class="small text-black-50">` +
+              `<a href="#">mystryssband@gmail.com</a>` +
+            `</div>` +
+          `</div>` +
+        `</div>` +
+      `</div>` +
+
+      `<div class="col-md-4 mb-3 mb-md-0">` +
+        `<div class="card py-4 h-100">` +
+          `<div class="card-body text-center">` +
+            `<i class="fas fa-mobile-alt text-primary mb-2"></i>` +
+            `<h4 class="text-uppercase m-0">Phone</h4>` +
+            `<hr class="my-4">` +
+            `<div class="small text-black-50">+1 (555) 123-4567</div>` +
+          `</div>` +
+        `</div>` +
+      `</div>` +
+    `</div>` +
+
+    `<div class="social d-flex justify-content-center">` +
+      `<a href="#" class="mx-2">` +
+        `<i class="fab fa-twitter"></i>` +
+      `</a>` +
+      `<a href="https://www.facebook.com/Mystryss-2080397102211829/" class="mx-2">` +
+        `<i class="fab fa-facebook-f"></i>` +
+      `</a>` +
+      `<a href="https://www.instagram.com/mystryssband/" class="mx-2">` +
+        `<i class="fab fa-instagram"></i>` +
+      `</a>` +
+     `</div>` +
+
+  `</div>` +
+`</section>`;
+
+document.getElementById("contact_section").innerHTML = insert;
